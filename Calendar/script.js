43dfebd7e646782daaeafb78ab71785a4586921b
Jonathan Martin <jonnymartin93@gmail.com>
2023-02-11 11:34:25 -0600

    added example

--- Calendar/script.js
+++ Calendar/script.js
@@ -14,7 +14,11 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  var now = dayjs();
+$('#1a').text(today.format('MMM D, YYYY'));
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
